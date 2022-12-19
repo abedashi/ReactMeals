e67fad26fde75fa81666aacfa7de6aaeea3271dd
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-12-19 18:44:33 +0200

    add animation on each added item

--- Food-Order-App/src/components/Layout/HeaderButton/HeaderCartButton.jsx
+++ Food-Order-App/src/components/Layout/HeaderButton/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../../context/cart-context";
 
 import CartIcon from "../../Cart/CartIcon";
@@ -6,13 +6,34 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = ({ onClick }) => {
   const context = useContext(CartContext);
+  const { items } = context;
+  const [btnIsHighligghted, setBtnIsHighligghted] = useState(false);
 
-  const numberOfCartItems = context.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighligghted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighligghted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighligghted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
