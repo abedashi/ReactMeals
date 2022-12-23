8c0522213ac90dc07219993d8ead2ae9b01ec73e
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-12-24 01:36:24 +0200

    add clear itams function

--- Food-Order-App/src/context/CartProvider.jsx
+++ Food-Order-App/src/context/CartProvider.jsx
@@ -54,6 +54,10 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "CLEAR") {
+    return defaultCart;
+  }
+
   return defaultCart;
 };
 
@@ -67,11 +71,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR  " });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
